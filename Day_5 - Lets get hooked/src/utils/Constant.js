a2696a5ded547f60262b9e1c567372e65c95da79
ajithb2601 <144740617+AjithB-26@users.noreply.github.com>
2024-03-18 14:57:19 -0400

    code added

---
+++ Day_5 - Lets get hooked/src/utils/Constant.js
@@ -0,0 +1,3 @@
+export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL = "https://static.vecteezy.com/system/resources/previews/004/803/427/original/illustration-graphic-of-food-typography-logo-perfect-to-use-for-food-company-free-vector.jpg";
